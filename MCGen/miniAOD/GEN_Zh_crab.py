5346a2007cd8ac382ee28c5c9bea474a0b8af911
John <jstupak@fnal.gov>
2015-02-06 18:23:27 -0600

    updated GEN files

--- MCGen/miniAOD/GEN_Zh_crab.py
+++ MCGen/miniAOD/GEN_Zh_crab.py
@@ -2,7 +2,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'GEN_Zh_checking2'
+config.General.requestName = 'GEN_Zh'
 
 config.section_("JobType")
 config.JobType.pluginName = 'PrivateMC'
@@ -10,27 +10,25 @@ config.JobType.generator = 'lhe'
 config.JobType.psetName = 'step1.py'
 config.JobType.allowNonProductionCMSSW = True
 config.JobType.inputFiles = ['/eos/uscms/store/user/lpcmbja/noreplica/boostedGen/Zh/lhe/unweighted_events.1.lhe']
-                            # '/eos/uscms/store/user/lpcmbja/noreplica/boostedGen/Zh/lhe/unweighted_events.2.lhe',
-                            # '/eos/uscms/store/user/lpcmbja/noreplica/boostedGen/Zh/lhe/unweighted_events.3.lhe',
-                            # '/eos/uscms/store/user/lpcmbja/noreplica/boostedGen/Zh/lhe/unweighted_events.4.lhe',
-                            # '/eos/uscms/store/user/lpcmbja/noreplica/boostedGen/Zh/lhe/unweighted_events.5.lhe',
-                            # '/eos/uscms/store/user/lpcmbja/noreplica/boostedGen/Zh/lhe/unweighted_events.6.lhe',
-                            # '/eos/uscms/store/user/lpcmbja/noreplica/boostedGen/Zh/lhe/unweighted_events.7.lhe',
-                            # '/eos/uscms/store/user/lpcmbja/noreplica/boostedGen/Zh/lhe/unweighted_events.8.lhe',
-                            # '/eos/uscms/store/user/lpcmbja/noreplica/boostedGen/Zh/lhe/unweighted_events.9.lhe',
-                            # '/eos/uscms/store/user/lpcmbja/noreplica/boostedGen/Zh/lhe/unweighted_events.10.lhe']
-
+                             '/eos/uscms/store/user/lpcmbja/noreplica/boostedGen/Zh/lhe/unweighted_events.2.lhe',
+                             '/eos/uscms/store/user/lpcmbja/noreplica/boostedGen/Zh/lhe/unweighted_events.3.lhe',
+                             '/eos/uscms/store/user/lpcmbja/noreplica/boostedGen/Zh/lhe/unweighted_events.4.lhe',
+                             '/eos/uscms/store/user/lpcmbja/noreplica/boostedGen/Zh/lhe/unweighted_events.5.lhe',
+                             '/eos/uscms/store/user/lpcmbja/noreplica/boostedGen/Zh/lhe/unweighted_events.6.lhe',
+                             '/eos/uscms/store/user/lpcmbja/noreplica/boostedGen/Zh/lhe/unweighted_events.7.lhe',
+                             '/eos/uscms/store/user/lpcmbja/noreplica/boostedGen/Zh/lhe/unweighted_events.8.lhe',
+                             '/eos/uscms/store/user/lpcmbja/noreplica/boostedGen/Zh/lhe/unweighted_events.9.lhe',
+                             '/eos/uscms/store/user/lpcmbja/noreplica/boostedGen/Zh/lhe/unweighted_events.10.lhe']
+                            
 config.section_("Data")
-#config.Data.inputDataset = '/QCD_Pt-470to600_Tune4C_13TeV_pythia8/Spring14dr-castor_PU_S14_POSTLS170_V6-v1/AODSIM'
-#config.Data.dbsUrl = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/'
+config.Data.primaryDataset ='Zh_m125_PtZ-200'
 config.Data.splitting = 'EventBased'
-config.Data.unitsPerJob = 20
-config.Data.totalUnits = 5000
+config.Data.unitsPerJob = 250
+config.Data.totalUnits = 100000
 config.Data.publication = True
 config.Data.publishDBS = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter/'
-config.Data.publishDataName = 'Zh_pT200_GEN'
-#config.Data.outlfn = '/store/group/lpcmbja/noreplica/boostedGen/Zh/GEN/'
-config.Data.outLFN = '/store/user/bparida/GEN'
+config.Data.publishDataName = 'GEN'
+config.Data.outlfn = '/store/group/lpcmbja/noreplica/boostedGen/Zh/GEN/'
 config.Data.ignoreLocality = True
 
 config.section_("Site")
